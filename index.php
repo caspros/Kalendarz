dab91d672330223bf3ef709ce718dfb86def82c4
caspros <caspros97@gmail.com>
2019-12-07 21:59:24 +0100

    Stworzylem index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+	session_start();
+?>
+
+<html>
+<head>   
+<link href="calendar.css" type="text/css" rel="stylesheet" />
+<title>Kalendarz Kacper & Aleksander</title>
+</head>
+<body>
+
+
+<br><br>
+<form method="post" action="export.php">
+     <input type="submit" name="export" class="btn btn-success" value="Export" />
+ </form>
+
+</body>
+</html>     
+
+
