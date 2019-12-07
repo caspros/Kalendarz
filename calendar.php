4b154ad739bd9cfbb8e8c0bbf407e8191d48100b
caspros <caspros97@gmail.com>
2019-12-07 23:36:19 +0100

    Added 2 functions to calculate days in month and weeks

---
+++ calendar.php
@@ -0,0 +1,65 @@
+<?php
+class Calendar {  
+     
+    /**
+     * Constructor
+     */
+    public function __construct(){     
+        $this->naviHref = htmlentities($_SERVER['PHP_SELF']);
+    }
+     
+    /********************* PROPERTY ********************/  
+    private $dayLabels = array("Pon","Wto","Śro","Czw","Pt","Sob","Ndz");
+     
+    private $currentYear=0;
+     
+    private $currentMonth=0;
+     
+     
+    /**
+    * calculate number of weeks in a particular month
+    */
+    private function _weeksInMonth($month=null,$year=null){
+         
+        if( null==($year) ) {
+            $year =  date("Y",time()); 
+        }
+         
+        if(null==($month)) {
+            $month = date("m",time());
+        }
+         
+        // find number of days in this month
+        $daysInMonths = $this->_daysInMonth($month,$year);
+         
+        $numOfweeks = ($daysInMonths%7==0?0:1) + intval($daysInMonths/7);
+         
+        $monthEndingDay= date('N',strtotime($year.'-'.$month.'-'.$daysInMonths));
+         
+        $monthStartDay = date('N',strtotime($year.'-'.$month.'-01'));
+         
+        if($monthEndingDay<$monthStartDay){
+             
+            $numOfweeks++;
+         
+        }
+         
+        return $numOfweeks;
+    }
+ 
+    /**
+    * calculate number of days in a particular month
+    */
+    private function _daysInMonth($month=null,$year=null){
+         
+        if(null==($year))
+            $year =  date("Y",time()); 
+ 
+        if(null==($month))
+            $month = date("m",time());
+             
+        return date('t',strtotime($year.'-'.$month.'-01'));
+    }
+
+     
+}
